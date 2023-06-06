fddad21183b2b945727db0016d991a7508cc3708
rickymahendra01 <rickymahendra465@gmail.com>
2023-06-06 09:58:14 +0800

    folder baru

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,426 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Ustadz Studio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('frontend/fonts/icomoon/style.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/css/bootstrap-datepicker.css')}}">
+    
+    
+    <link rel="stylesheet" href="{{asset('frontend/fonts/flaticon/font/flaticon.css')}}">
+  
+    <link rel="stylesheet" href="{{asset('frontend/css/aos.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/css/jquery.fancybox.min.css')}}">
+    
+
+    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    
+    <div class="site-navbar-wrap">
+      
+      <div class="site-navbar site-navbar-target js-sticky-header">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-6 col-md-4">
+              <h1 class="my-0 site-logo"><a href="index.html">Ustadz<span class="text-primary">.</span> </a></h1>
+            </div>
+            <div class="col-6 col-md-8">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+
+                  <div class="d-inline-block d-lg-block ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black">
+                    <span class="icon-menu h3"></span> <span class="menu-text">Menu</span>
+                  </a></div>
+
+                  <ul class="site-menu main-menu js-clone-nav d-none d-lg-none">
+                    <li><a href="#home-section" class="nav-link">Home</a></li>
+                    <li><a href="#what-we-do-section" class="nav-link">What We Do</a></li>
+                    <li><a href="#about-section" class="nav-link">About Us</a></li>
+                    <li><a href="#portfolio-section" class="nav-link">Portfolio</a></li>
+                    <li><a href="#studio-section" class="nav-link">Gallery</a></li>
+                    <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- END .site-navbar-wrap -->
+    
+    <div class="site-blocks-cover" id="home-section">
+      <div class="img-wrap">
+        <div class="owl-carousel slide-one-item hero-slider">
+          <div class="slide">
+            <img src="{{asset('frontend/images/glint_hero_1.jpg')}}" alt="Image">  
+          </div>
+          <div class="slide">
+            <img src="{{asset('frontend/images/glint_hero_2.jpg')}}" alt="Image">  
+          </div>
+          <div class="slide">
+            <img src="{{asset('frontend/images/glint_hero_3.jpg')}}" alt="Image">  
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 ml-auto align-self-center">
+            <div class="intro">
+              <div class="heading">
+                <h1>Ustadz Studio</h1>
+              </div>
+              <div class="text">
+                <p class="sub-text mb-5">Menyampaikan kebenaran dan menyelamatkan ummat dari segala bentuk bid'ah</p>
+                <p><a href="#" target="_blank" class="btn btn-outline-primary btn-md">Login</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- END .site-blocks-cover -->
+
+    <div class="site-section" id="what-we-do-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-lg-6 section-title">
+            <span class="sub-title mb-2 d-block">Kami Menyedikan</span>
+            <h2 class="title text-primary">Far far away, behind the word mountains far from the countries</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-10 ml-auto">
+            <div class="row">
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <h3>Al-Qur'an</h3>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p><a href="#" class="readmore">Learn more</a></p>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <h3>Artikel</h3>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p><a href="#" class="readmore">Learn more</a></p>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <h3>Poster Dakwah</h3>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p><a href="#" class="readmore">Learn more</a></p>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <h3>Nasehat</h3>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p><a href="#" class="readmore">Learn more</a></p>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <h3>Jadwal Kajian</h3>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p><a href="#" class="readmore">Learn more</a></p>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="service h-100">
+                  <h3>Video Dakwah</h3>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p><a href="#" class="readmore">Learn more</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- END .site-section -->
+
+    <div class="site-section" id="about-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 mb-5">
+            <img src="{{asset('frontend/images/glint_about_1.jpg')}}" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-lg-5 ml-auto section-title">
+            <span class="sub-title mb-2 d-block">Acquaint Us</span>
+            <h2 class="title text-primary mb-3">About us</h2>
+            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+
+
+            <div class="d-flex">
+              <ul class="list-unstyled ul-check success mr-5">
+                <li>Al-Qur'an</li>
+                <li>Artikel</li>
+                <li>Poster Dakwah</li>
+                
+              </ul>
+              <ul class="list-unstyled ul-check success">
+                
+                <li>Nasehat</li>
+                <li>Jadwal Kajian</li>
+                <li>Video Dakwah</li>
+              </ul>
+            </div>
+            <p><a data-fancybox data-ratio="2" href="https://vimeo.com/326176805" class="d-flex align-items-center"><span class="icon-play_circle_outline h4 m-0 mr-2"></span> <span>Watch the video</span></a></p>
+          </div>
+        </div>
+      </div>
+    </div> <!-- .END site-section -->
+
+
+    
+    <div class="site-section" id="studio-section">
+      <div class="container">
+
+        <div class="row mb-5 ">
+          <div class="col-md-7 section-title text-center mx-auto">
+            <span class="sub-title mb-2 d-block">Photos</span>
+            <h2 class="title text-primary mb-3">Studio Gallery</h2>
+          </div>
+        </div>
+       
+        <div id="posts" class="row no-gutter">
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_1.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_1.jpg')}}">
+            </a>
+          </div>
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_2.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_2.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_3.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_3.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_4.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_4.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_5.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_5.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_6.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_6.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_7.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_7.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_9.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_9.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_10.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_10.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_11.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_11.jpg')}}">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="{{asset('frontend/images/sq_img_12.jpg')}}" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="{{asset('frontend/images/sq_img_12.jpg')}}">
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+    </div> <!-- END .site-section -->
+    
+    <div class="site-section" id="contact-section">
+      <div class="container">
+        <form action="" class="contact-form">
+
+          <div class="section-title text-center mb-5">
+            <span class="sub-title mb-2 d-block">Get In Touch</span>
+            <h2 class="title text-primary">Contact Us</h2>
+          </div>
+
+          <div class="row mb-4">
+            <div class="col-md-6 mb-4 mb-md-0">
+              <input type="text" class="form-control" placeholder="First name">
+            </div>
+            <div class="col-md-6">
+              <input type="text" class="form-control" placeholder="Last name">
+            </div>
+          </div>
+
+          <div class="row mb-4">
+            <div class="col-12">
+              <input type="text" class="form-control" placeholder="Email">
+            </div>
+          </div>
+
+          <div class="row mb-4">
+            <div class="col-12">
+              <textarea class="form-control" name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary btn-md">Send Message</button>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </div> <!-- END .site-section -->
+  
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <div class="row mb-5">
+              <div class="col-12">
+                <h3 class="footer-heading mb-4">About Us</h3>
+                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+              </div>
+            </div>
+            
+
+            
+          </div>
+          <div class="col-lg-3 ml-auto">
+           
+            <div class="row mb-5">
+              <div class="col-md-12">
+                <h3 class="footer-heading mb-4">Navigation</h3>
+              </div>
+              <div class="col-md-6 col-lg-6">
+                <ul class="list-unstyled">
+                  <li><a href="#home-section" class="smoothscroll">Home</a></li>
+                  <li><a href="#what-we-do-section" class="smoothscroll">What We Do</a></li>
+                  <li><a href="#about-section" class="smoothscroll">About Us</a></li>
+                  <li><a href="#team-section"  class="smoothscroll">Team</a></li>
+                </ul>
+              </div>
+              
+            </div>
+            
+          </div>
+          
+
+          <div class="col-lg-4 mb-5 mb-lg-0">
+
+            <div class="mb-5">
+              <h3 class="footer-heading mb-2">Subscribe Newsletter</h3>
+              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+
+              <form method="post" class="form-subscribe">
+                <div class="form-group mb-3">
+                  <input type="text" class="form-control border-white text-white bg-transparent" placeholder="Enter full name" aria-label="Enter Email" aria-describedby="button-addon2">
+                </div>
+                <div class="form-group mb-3">
+                  <input type="text" class="form-control border-white text-white bg-transparent" placeholder="Enter email" aria-label="Enter Email" aria-describedby="button-addon2">
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-primary px-5" type="submit">Subscribe</button>
+                </div>
+              </form>
+
+            </div>
+
+            
+
+
+          </div>
+          
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="mb-4">
+                  <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                  <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                  <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                  <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                </div>
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Create By Ricky Mahendra</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="{{asset('frontend/js/jquery-3.3.1.min.js')}}"></script>
+  <script src="{{asset('frontend/js/popper.min.js')}}"></script>
+  <script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('frontend/js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('frontend/js/aos.js')}}"></script>
+  <script src="{{asset('frontend/js/jquery.sticky.js')}}"></script>
+  <script src="{{asset('frontend/js/stickyfill.min.js')}}"></script>
+  <script src="{{asset('frontend/js/jquery.easing.1.3.js')}}"></script>
+  <script src="{{asset('frontend/js/isotope.pkgd.min.js')}}"></script>
+  
+  <script src="{{asset('frontend/js/jquery.fancybox.min.js')}}"></script>
+  <script src="{{asset('frontend/js/main.js')}}"></script>
+
+     
+  </body>
+</html>
